0e5cb405b086f34cddc31dd0f23c8550373c9beb
Lyubomir <lyubomir.hristov3@gmail.com>
2020-05-01 23:58:09 +0100

    Finished query optimisation.

--- src/sjdb/Optimiser.java
+++ src/sjdb/Optimiser.java
@@ -6,6 +6,7 @@ import javax.smartcardio.ATR;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("Duplicates")
 public class Optimiser {
     private Catalogue catalogue;
 
@@ -14,8 +15,10 @@ public class Optimiser {
     }
 
     public Operator optimise(Operator op){
+        // Get all operators from the canonical plan
         List<Operator> operators = getOperators(op, new ArrayList<>());
 
+        // Separate each operator type
         List<Scan> scanList = new ArrayList<>();
         List<Product> productList = new ArrayList<>();
         List<Select> selectList = new ArrayList<>();
@@ -35,7 +38,7 @@ public class Optimiser {
 
         List<Operator> optimisedScanList = new ArrayList<>();
 
-        // Move select in the form attr=val
+        // Move selects in the form attr=val above the corresponding Relations
         for(Scan scan: scanList){
             Operator optimisedScan = scan;
 
@@ -48,12 +51,12 @@ public class Optimiser {
             optimisedScanList.add(optimisedScan);
         }
 
-        List<Operator> optimisedProductList = new ArrayList<>();
-
         // Reorder joins
 
-        // Make all pairs for product 1
 
+        List<Operator> optimisedProductList = new ArrayList<>();
+
+        // Make all possible pairs for product 1
         List<List<Operator>> pairs = new ArrayList<>();
 
         for(int i = 0; i < optimisedScanList.size() - 1; i++){
@@ -65,49 +68,123 @@ public class Optimiser {
             }
         }
 
-        // Create product 1
+        // Get all restricting attr in order to move Project operators
+        List<Attribute> selectAndProjectAttributes = new ArrayList<>();
+
+        for(Select select: selectList){
+            if(!select.getPredicate().equalsValue()){
+                if(!selectAndProjectAttributes.contains(select.getPredicate().getLeftAttribute())){
+                    selectAndProjectAttributes.add(select.getPredicate().getLeftAttribute());
+                }
+
+                if(!selectAndProjectAttributes.contains(select.getPredicate().getRightAttribute())){
+                    selectAndProjectAttributes.add(select.getPredicate().getRightAttribute());
+                }
+            }
+        }
+
+        for(Project project: projectList){
+            for(Attribute attr: project.getAttributes()){
+                if(!selectAndProjectAttributes.contains(attr)){
+                    selectAndProjectAttributes.add(attr);
+                }
+            }
+        }
+
+        // Create all products
 
         for(int p = 0; p < productList.size(); p++){
             if(p == 0){
+                // Create the best first product
+
                 Operator bestProduct = null;
-                int bestOutputSize = -1;
+                int bestOutputSize = Integer.MAX_VALUE;
                 List<Operator> inputsUsed = new ArrayList<>();
 
+                // The attributes of a join should be removed after the join from the list with possible attributes to project
+                Attribute leftPredicate = null;
+                Attribute rightPredicate = null;
+
+                // Estimate cost of product for each pair of inputs
                 for(List<Operator> pair: pairs){
                     Operator product = new Product(pair.get(0), pair.get(1));
                     List<Attribute> productAttributes = getAttributes(product, new ArrayList<>());
 
+                    Operator left = ((Product) product).getLeft();
+                    Operator right = ((Product) product).getRight();
+
+                    List<Attribute> leftAttributes = getAttributes(left, new ArrayList<>());
+                    List<Attribute> rightAttributes = getAttributes(right, new ArrayList<>());
+
+                    boolean leftCanBeOptimised = false;
+                    boolean rightCanBeOptimised = false;
+
+                    List<Attribute> leftProjectAttributes = new ArrayList<>();
+                    List<Attribute> rightProjectAttributes = new ArrayList<>();
+
+                    // Check if a Project can be put above each child node to limit size
+                    for(Attribute attr: selectAndProjectAttributes){
+                        if(leftAttributes.contains(attr)){
+                            leftProjectAttributes.add(attr);
+                            leftCanBeOptimised = true;
+                        }else if(rightAttributes.contains(attr)){
+                            rightProjectAttributes.add(attr);
+                            rightCanBeOptimised =  true;
+                        }
+                    }
+
+                    // if a project can be put and the project does not contain all possible attributes of the child node
+                    // and if it is not the case that all attributes are projected in the end
+                    if(leftCanBeOptimised && leftAttributes.size() != leftProjectAttributes.size() && !projectList.isEmpty()){
+                        left = new Project(left, leftProjectAttributes);
+                    }
+
+                    if(rightCanBeOptimised && rightAttributes.size() != rightProjectAttributes.size() && !projectList.isEmpty()){
+                        right = new Project(right, rightProjectAttributes);
+                    }
+
                     // Match select in the form attr=attr with product to estimate cost
                     for(Select select: selectList){
+
+                        // if a select can be put on top of the product
                         if(!select.getPredicate().equalsValue() && productAttributes.contains(select.getPredicate().getLeftAttribute()) && productAttributes.contains(select.getPredicate().getRightAttribute())){
-                            if(product instanceof Product){
-                                product = new Join(((Product) product).getLeft(), ((Product) product).getRight(), select.getPredicate());
-                            }else{
-                                product = new Select(product, select.getPredicate());
-                            }
+
+                            // make sure the predicate is not reversed
+                            Predicate predicate = leftAttributes.contains(select.getPredicate().getLeftAttribute()) ? select.getPredicate() : new Predicate(select.getPredicate().getRightAttribute(), select.getPredicate().getLeftAttribute());
+
+                            product = new Join(left, right, predicate);
+                            leftPredicate = predicate.getLeftAttribute();
+                            rightPredicate = predicate.getRightAttribute();
                         }
                     }
 
                     Estimator estimator = new Estimator();
                     product.accept(estimator);
 
-                    if(product.getOutput().getTupleCount() > bestOutputSize){
+                    // if the cost of the current relation is less than the best one so far, make the current one best
+                    if(product.getOutput().getTupleCount() < bestOutputSize){
                         bestProduct = product;
                         bestOutputSize = product.getOutput().getTupleCount();
                         inputsUsed = pair;
+                        selectAndProjectAttributes.remove(leftPredicate);
+                        selectAndProjectAttributes.remove(rightPredicate);
                     }
                 }
 
                 bestProduct.setOutput(null);
                 optimisedProductList.add(bestProduct);
+
+                // The used inputs should be removed from the possible inputs to create a future join/product
                 optimisedScanList.remove(inputsUsed.get(0));
                 optimisedScanList.remove(inputsUsed.get(1));
             }else{
                 // for every next product match previous product with the best relation
 
                 Operator bestProduct = null;
-                int bestOutputSize = -1;
+                int bestOutputSize = Integer.MAX_VALUE;
                 Operator inputUsed = null;
+                Attribute leftPredicate = null;
+                Attribute rightPredicate = null;
 
 
                 for(int i = 1; i < productList.size(); i++){
@@ -116,23 +193,57 @@ public class Optimiser {
                         inputUsed = optimisedScan;
                         List<Attribute> productAttributes = getAttributes(product, new ArrayList<>());
 
+                        Operator left = product.getInputs().get(0);
+                        Operator right = product.getInputs().get(1);
+
+                        List<Attribute> leftAttributes = getAttributes(left, new ArrayList<>());
+                        List<Attribute> rightAttributes = getAttributes(right, new ArrayList<>());
+
+                        boolean leftCanBeOptimised = false;
+                        boolean rightCanBeOptimised = false;
+
+                        List<Attribute> leftProjectAttributes = new ArrayList<>();
+                        List<Attribute> rightProjectAttributes = new ArrayList<>();
+
+                        // Move projects
+                        for(Attribute attr: selectAndProjectAttributes){
+                            if(leftAttributes.contains(attr)){
+                                leftProjectAttributes.add(attr);
+                                leftCanBeOptimised = true;
+                            }else if(rightAttributes.contains(attr)){
+                                rightProjectAttributes.add(attr);
+                                rightCanBeOptimised =  true;
+                            }
+                        }
+
+                        if(leftCanBeOptimised && leftAttributes.size() != leftProjectAttributes.size() && !projectList.isEmpty()){
+                            left = new Project(left, leftProjectAttributes);
+                        }
+
+                        if(rightCanBeOptimised && rightAttributes.size() != rightProjectAttributes.size() && !projectList.isEmpty()){
+                            right = new Project(right, rightProjectAttributes);
+                        }
+
                         // Match select in the form attr=attr with product to estimate cost
                         for(Select select: selectList){
                             if(!select.getPredicate().equalsValue() && productAttributes.contains(select.getPredicate().getLeftAttribute()) && productAttributes.contains(select.getPredicate().getRightAttribute())){
-                                if(product instanceof Product){
-                                    product = new Join(((Product) product).getLeft(), ((Product) product).getRight(), select.getPredicate());
-                                }else{
-                                    product = new Select(product, select.getPredicate());
-                                }
+                                // make sure the predicate is not reversed
+                                Predicate predicate = leftAttributes.contains(select.getPredicate().getLeftAttribute()) ? select.getPredicate() : new Predicate(select.getPredicate().getRightAttribute(), select.getPredicate().getLeftAttribute());
+
+                                product = new Join(left, right, predicate);
+                                leftPredicate = predicate.getLeftAttribute();
+                                rightPredicate = predicate.getRightAttribute();
                             }
                         }
 
                         Estimator estimator = new Estimator();
                         product.accept(estimator);
 
-                        if(product.getOutput().getTupleCount() > bestOutputSize){
+                        if(product.getOutput().getTupleCount() < bestOutputSize){
                             bestProduct = product;
                             bestOutputSize = product.getOutput().getTupleCount();
+                            selectAndProjectAttributes.remove(leftPredicate);
+                            selectAndProjectAttributes.remove(rightPredicate);
                         }
                     }
 
@@ -164,6 +275,9 @@ public class Optimiser {
         if(op instanceof Product){
             result = getAttributes(((Product) op).getLeft(), result);
             result = getAttributes(((Product) op).getRight(), result);
+        }else if(op instanceof Join){
+            result = getAttributes(((Join) op).getLeft(), result);
+            result = getAttributes(((Join) op).getRight(), result);
         }else{
             result = getAttributes(op.getInputs().get(0), result);
         }
@@ -172,12 +286,12 @@ public class Optimiser {
     }
 
 
+    // Assumes no joins in canonical plan
     private List<Operator> getOperators(Operator op, List<Operator> result){
         if(op instanceof Scan){
             result.add(op);
             return result;
         }
-
 
         if(op instanceof Product){
             result = getOperators(((Product) op).getLeft(), result);
@@ -188,93 +302,7 @@ public class Optimiser {
             result.add(op);
         }
 
-
         return result;
-
     }
 }
 
-
-
-
-//if(op instanceof Scan){
-//        return op;
-//        }
-//
-//        List<Operator> operators = getOperators(op, new ArrayList<>());
-//        List<Select> selectOperators = new ArrayList<>();
-//
-//        for(Operator operator: operators){
-//        if(operator instanceof Select){
-//        selectOperators.add((Select) operator);
-//        }
-//        }
-//
-//        Operator optimisedPlan = null;
-//        List<Operator> children = new ArrayList<>();
-//
-//        for(int i = 0; i <= operators.size() - 1; i++){
-//        Operator currentOperator = operators.get(i);
-//
-//        if(currentOperator instanceof Scan){
-//        for(Select select: selectOperators){
-//        if(select.getPredicate().equalsValue() && ((Scan)currentOperator).getRelation().getAttributes().contains(select.getPredicate().getLeftAttribute())){
-//        currentOperator = new Select(currentOperator, select.getPredicate());
-//        }
-//        }
-//
-//        children.add(currentOperator);
-//        }else if(currentOperator instanceof Product){
-//        optimisedPlan = new Product(children.get(0), children.get(1));
-//
-//        for(Select select: selectOperators){
-//        List<Attribute> currentOperatorAttributes = getAttributes(currentOperator, new ArrayList<>());
-//
-//        if(!select.getPredicate().equalsValue() && currentOperatorAttributes.contains(select.getPredicate().getLeftAttribute()) && currentOperatorAttributes.contains(select.getPredicate().getRightAttribute())){
-//        optimisedPlan = new Select(optimisedPlan, select.getPredicate());
-//        }
-//        }
-//
-//        children.clear();
-//        children.add(optimisedPlan);
-//        }else if(currentOperator instanceof Select){
-//        if(!shouldSkipSelect(optimisedPlan, (Select) currentOperator)){
-//        if(optimisedPlan == null){
-//        optimisedPlan = children.get(0);
-//        }else{
-//        optimisedPlan = new Select(optimisedPlan, ((Select) currentOperator).getPredicate());
-//        }
-//        }
-//        }else if(currentOperator instanceof Project){
-//        optimisedPlan = new Project(optimisedPlan, ((Project) currentOperator).getAttributes());
-//        }
-//        }
-//
-//        return optimisedPlan;
-
-
-//    private boolean shouldSkipSelect(Operator optimisedPlan, Select selectToAdd){
-//        if(optimisedPlan == null){
-//            return false;
-//        }
-//
-//        List<Operator> operators = getOperators(optimisedPlan, new ArrayList<>());
-//        List<Select> selectOperators = new ArrayList<>();
-//
-//        for(Operator operator: operators){
-//            if(operator instanceof Select){
-//                selectOperators.add((Select) operator);
-//            }
-//        }
-//
-//        boolean shouldSkipSelect = false;
-//
-//        for(Select existingSelect: selectOperators){
-//            if(selectToAdd.getPredicate().equals(existingSelect.getPredicate())){
-//                shouldSkipSelect = true;
-//            }
-//        }
-//
-//        return shouldSkipSelect;
-//    }
-//
